4cca9f77b5d125988c6c64eaac5409839f398cea
cgm <caogaoming@qccr.com>
2018-11-30 10:36:28 +0800

    提交！

---
+++ carroom-facade/src/main/java/org/carroom/facade/App.java
@@ -0,0 +1,13 @@
+package org.carroom.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
